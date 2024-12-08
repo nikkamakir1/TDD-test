d3d1f090108ce0829a56fa132e308fa549d97fb2
nikkamakir1 <takamasatabata8@gmail.com>
2024-12-08 17:54:18 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+def double_number(number: int) -> int:
+    """
+    Returns the double of the given number.
+    :param number: The number to double.
+    :return: The doubled number.
+    """
+    return number * 2
