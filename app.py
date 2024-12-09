d6de9075b668b8bed94b9133e9129b448ff416e0
nikkamakir1 <takamasatabata8@gmail.com>
2024-12-09 14:33:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import time
 def Soup(number: int):
-    current_time = time.time + 32400;
+    current_time = time.time + 32400
     #UTCからJSTに変換
-    current_day = current_time/86400;
-    Today_menu_number = (current_day -19936)%4;
+    current_day = current_time/86400
+    Today_menu_number = (current_day -19936)%4
     Today_menu_number += number
     if menu_number == 0 :
-        printf("Today's Soup is Soy Sauce Seaweed\n");
-    }
+        return "Soy Sauce Seaweed"
     if menu_number == 1 :
-        printf("Today's Soup is Salt Seaweed\n");
-    } 
+        return "Salt Seaweed!!!"
     if menu_number == 2 :
-        printf("Today's Soup is Soy Sauce Egg\n");
-    } 
+        return "Soy Sauce Egg"
     if menu_number == 3 :
-        printf("Today's Soup is Salt Egg\n");
-    }   
-    return 0;
-}
+        return "Salt Egg!!!"
+
