4eefbda37a94244c77edfdd7d918c26dbb9797e2
nikkamakir1 <takamasatabata8@gmail.com>
2024-12-09 16:01:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ def soup(number):
     #UTCからJSTに変換
     current_day = current_time//86400
     menu_number = (current_day + number -19936)%4
-    if menu_number == 0 :
+    if menu_number == 0:
         result = "Soy Sauce Seaweed"
-    if menu_number == 1 :
+    if menu_number == 1:
         result = "Salt Seaweed!!!"
-    if menu_number == 2 :
+    if menu_number == 2:
         result = "Soy Sauce Egg"
-    if menu_number == 3 :
+    if menu_number == 3:
         result = "Salt Egg!!!"
     return result
