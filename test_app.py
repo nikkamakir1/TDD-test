ba00ccdb262943e4899e0d6b6b8502eeb9043121
nikkamakir1 <takamasatabata8@gmail.com>
2024-12-09 16:14:27 +0900

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,4 +6,4 @@ def test_soup():
     assert soup(-2) == 'Soy Sauce Seaweed'  # 負の値
     assert soup(1) == 'Salt Egg!!!'  # 正の値
     assert soup(365) == 'Salt Egg!!!'  # 大きな正の値
-    assert soup(-365) == 'Sal Seaweed!!!'  # 負の大きな値
+    assert soup(-365) == 'Salt Seaweed!!!'  # 負の大きな値
