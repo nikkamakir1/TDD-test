6ae1d05465c6f4c773bf1ec71ab2a359e0f85154
nikkamakir1 <takamasatabata8@gmail.com>
2024-12-09 16:11:10 +0900

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import soup
 
 
-def soup():
+def test_soup():
     assert soup(0) == 'Soy Sauce Egg'  # 今日のスープ
     assert soup(-2) == 'Soy Sauce Seaweed'  # 負の値
     assert soup(1) == 'Salt Egg!!!'  # 正の値
